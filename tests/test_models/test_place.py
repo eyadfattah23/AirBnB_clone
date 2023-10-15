b4706a586ded9102f44b0849abc3a996ecd63118
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-10-16 02:45:15 +0300

    class created

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -6,3 +6,16 @@ import unittest
 import datetime
 """define test class to test base model"""
 
+
+class TestPlace(unittest.TestCase):
+    """test class for testing BaseModel"""
+
+    def setUp(self):
+        """set up module"""
+        self.my_model = Place()
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+        self.my_model_json = self.my_model.to_dict()
+
+        self.args_base = Place(89, 'my_model', 0)
+        self.base_with_kwargs = Place(**self.my_model_json)
