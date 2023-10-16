e96e3d7d92c814ba331afea4948aae4345d7338e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-16 03:31:46 +0300

    Console 1.0

--- console.py
+++ console.py
@@ -4,6 +4,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 from models import storage
 
@@ -14,7 +19,8 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb)'
 
-    __classes_names = ['BaseModel', 'User']
+    __classes_names = ['BaseModel', 'User', 'Place',
+                       'State', 'City', 'Amenity', 'Review']
 
     def emptyline(self):
         '''overriding the emptyline method.
