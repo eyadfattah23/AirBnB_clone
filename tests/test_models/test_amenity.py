98c912fa4db69623fb37361ec469ed2cf50e054e
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-10-16 03:04:01 +0300

    test file created

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+import unittest
+import datetime
+"""define test class to test base model"""
+
+
+class TestState(unittest.TestCase):
+    """test class for testing BaseModel"""
+
+    def setUp(self):
+        """set up module"""
+        self.my_model = Amenity()
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+        self.my_model_json = self.my_model.to_dict()
+
+        self.args_base = Amenity(89, 'my_model', 0)
+        self.base_with_kwargs = Amenity(**self.my_model_json)
